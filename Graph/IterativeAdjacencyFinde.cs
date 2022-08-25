81bab7383c80993aaa410fec3be3be89160c5ba3
MehdiSaadati <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-25 11:22:42 +0430

    Add files via upload

---
+++ Graph/IterativeAdjacencyFinde.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class IterativeAdjacencyFinde
+    {
+        public void Find()
+        {
+            int node = int.Parse(Console.ReadLine());
+            int[,] numArray = new int[node, node];
+
+            for (int i = 0; i < node; i++)
+            {
+                for (int j = 0; j < node; j++)
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    numArray[i, j] = num;
+                }
+            }
+
+            int firstChar = 65;
+            int secondChar = 65;
+
+            for (int i = 0; i < node; i++)
+            {
+                for (int j = 0; j < node; j++)
+                {
+                    int temp = numArray[i, j];
+                    if (temp == 1)
+                    {
+                        Console.WriteLine($"{(char)firstChar} -> {(char)secondChar}");
+                    }
+                    secondChar++;
+                }
+                firstChar++;
+                secondChar = 65;
+                Console.WriteLine("---");
+            }
+        }
+    }
+}
